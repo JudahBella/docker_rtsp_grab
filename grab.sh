472c9417f1ff3fa1b1b33a6aab603d8937cfbce2
JudahBella Teng <68019824+JudahBella@users.noreply.github.com>
2023-06-21 00:18:11 +0800

    enable hwaccel

--- grab.sh
+++ grab.sh
@@ -2,4 +2,4 @@
 /usr/sbin/lighttpd -D -f /lighttpd.conf &
 url=$URL
 fps=$FPS
-ffmpeg -loglevel error -y -i $url -vf fps=$fps -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot.jpg
+ffmpeg -loglevel error -hwaccel auto -y -i $url -vf fps=$fps -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot.jpg
