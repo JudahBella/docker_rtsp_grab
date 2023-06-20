96c7fd2c23d8942067cda3667a3f7de4869b707d
JudahBella Teng <68019824+JudahBella@users.noreply.github.com>
2023-06-20 23:09:14 +0800

    include ability to adjust FPS

--- grab.sh
+++ grab.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 /usr/sbin/lighttpd -D -f /lighttpd.conf &
 url=$URL
-ffmpeg -loglevel error -y -i $url -r 1 -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot.jpg
+fps=$FPS
+ffmpeg -loglevel error -y -i $url -vf fps=$fps -vsync 1 -qscale 1 -f image2 -updatefirst 1 /tmp/www/snapshot.jpg
